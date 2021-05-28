dd9d16e0ec451f7e138bba4c22f55380f17fb470
AtmosOne <adil.rashitov.98@gmail.com>
2021-05-29 01:44:42 +0600

    Docker io installation.

--- prepare.sh
+++ prepare.sh
@@ -15,6 +15,8 @@ sudo add-apt-repository \
    stable";
 sudo apt-get update;
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose;
+sudo apt install docker.io;
+
 
 # 4. Python3
 sudo add-apt-repository ppa:deadsnakes/ppa;
